771c21802563fef513a1280ba7a12188e01676e6
Frenox <maxime.huet15@gmail.com>
2022-04-29 08:49:34 +0200

    Removed UE5 version
    Removed the UE5 version of the project that was here for no reason

--- The_Omnipresence 5.0/Source/The_Omnipresence/The_Omnipresence.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "The_Omnipresence.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, The_Omnipresence, "The_Omnipresence" );
- 
